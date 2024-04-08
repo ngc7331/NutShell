7ce33c4aa669d25ac7d29555355ab81f612a4d8b
ngc7331 <ngc7331@outlook.com>
2024-04-08 13:03:10 +0800

    fix: BBGSharePredictor wrapper

--- src/main/scala/nutcore/frontend/BPU/BlackBoxPredictor.scala
+++ src/main/scala/nutcore/frontend/BPU/BlackBoxPredictor.scala
@@ -40,55 +40,58 @@ class BB2bcImp extends BlackBox() with HasBlackBoxResource {
   addResource("/BB2bcImp.v")
 }
 
-class BBGSharePredictorImp_BSD_sim extends BlackBox() with HasBlackBoxResource {
+class BBGSharePredictorImp_BSD extends BlackBox() with HasBlackBoxResource {
   val io = IO(new Bundle {
     val pc = Input(UInt(32.W))
     val train_pc = Input(UInt(32.W))
     val train_taken = Input(Bool())
     val pht_rdata = Input(UInt(2.W))
     val ghr_rdata = Input(UInt(16.W))
+    val train_ghr_rdata = Input(UInt(16.W))
     val taken = Output(Bool())
     val pht_wdata = Output(Bool())
     val pht_raddr = Output(UInt(9.W))
     val pht_waddr = Output(UInt(9.W))
     val ghr_wdata = Output(Bool())
   }).suggestName("io")
+}
+
+class BBGSharePredictorImp_BSD_sim extends BBGSharePredictorImp_BSD {
   addResource("/nutcore/frontend/BPU/BBGSharePredictorImp_BSD_sim.v")
 }
 
-class BBGSharePredictor extends Predictor { // wrapper
-  val pht = RegInit(VecInit(Seq.fill(512)(2.U(2.W)))) // weak taken
+class BBGSharePredictorImp_BSD_NutShell extends BBGSharePredictorImp_BSD {
+  addResource("/nutcore/frontend/BPU/BBGSharePredictorImp_BSD_NutShell.v")
+}
+
+/* wrapper */
+class BBGSharePredictor extends Predictor {
+  val pht = Mem(NRSetPHT, UInt(SatLength.W))
   val ghr = RegInit(0.U(16.W))
 
-  val imp = Module(new BBGSharePredictorImp_BSD_sim)
-  imp.io.pc := RegEnable(io.pc.bits, io.pc.valid) // latch 1 cycle to sync with btbread
-  imp.io.pht_rdata := RegEnable(pht(imp.io.pht_raddr), io.pc.valid)
-  imp.io.ghr_rdata := RegEnable(ghr, io.pc.valid)
+  val imp = Module(new BBGSharePredictorImp_BSD_NutShell)
+  imp.io.pc := RegNext(io.pc.bits) // latch 1 cycle to sync with btbread
+  imp.io.pht_rdata := RegNext(pht.read(imp.io.pht_raddr))
+  imp.io.ghr_rdata := RegNext(ghr)
 
   imp.io.train_pc := io.update.pc
   imp.io.train_taken := io.update.actualTaken
-
-  when (RegNext(io.pc.valid)) {
-    printf("pc=%x, pht_raddr=%x, pht_rdata=%x, ghr_rdata=%x, taken=%x\n", imp.io.pc, imp.io.pht_raddr, imp.io.pht_rdata, imp.io.ghr_rdata, imp.io.taken)
-    printf("want         pht_raddr=%x,                              taken=%x\n",
-      ((imp.io.ghr_rdata(15,12) ^ imp.io.ghr_rdata(11,8) ^ imp.io.ghr_rdata(7,4) ^ imp.io.ghr_rdata(3,0)) << 5) ^ imp.io.pc(10,2),
-      imp.io.pht_rdata(1)
-    )
-  }
+  imp.io.train_ghr_rdata := io.update.meta.ghr
 
   when (io.btb.entry._type === BTBtype.B) {
     io.out.valid := imp.io.taken
-    io.out.meta.ghr := RegEnable(ghr, io.pc.valid)
+    io.out.meta.ghr := RegNext(ghr)
   }
 
   // PHT update
+  val cnt = RegNext(pht.read(imp.io.pht_waddr))
   val updateLatch = RegNext(io.update)
-  when (io.update.valid && ALUOpType.isBranch(io.update.fuOpType)) {
+  when (updateLatch.valid && ALUOpType.isBranch(updateLatch.fuOpType)) {
     val sat = Module(new BB2bcImp())
-    sat.io.x := pht(imp.io.pht_waddr)
-    sat.io.c := imp.io.pht_wdata
-    pht(imp.io.pht_waddr) := sat.io.y
-    BPUDebug(true.B, "[%d] PHT update pc=%x, cnt=%x->%x\n", GTimer(), updateLatch.pc, pht(imp.io.pht_waddr), sat.io.y)
+    sat.io.x := cnt
+    sat.io.c := RegNext(imp.io.pht_wdata)
+    pht.write(RegNext(imp.io.pht_waddr), sat.io.y)
+    BPUDebug(true.B, "[%d] PHT update pc=%x, cnt=%x->%x\n", GTimer(), updateLatch.pc, cnt, sat.io.y)
   }
 
   // GHR update
@@ -97,13 +100,31 @@ class BBGSharePredictor extends Predictor { // wrapper
   when (io.update.valid && ALUOpType.isBranch(io.update.fuOpType) && io.update.isMissPredict) {
     // NOTE: on a miss predict, redirect will be issued by wbu (1 cycle later than alu, and therefore, BPUUpdateReq)
     //       so, when the redirected pc is sent from ifu, ghr is already updated, that's good
-    new_ghr := Cat(io.update.meta.ghr, imp.io.ghr_wdata)
+    new_ghr := Cat(imp.io.train_ghr_rdata, imp.io.ghr_wdata)
     BPUDebug(true.B, "[%d] GHR update miss pc=%x ghr=%x->%x, predicted at %d\n", GTimer(), io.update.pc, ghr, new_ghr, io.update.meta.time)
   }
   // speculative update
   .elsewhen (io.btb.hit && io.btb.entry._type === BTBtype.B) {
     new_ghr := Cat(ghr, imp.io.taken)
-    BPUDebug(true.B, "[%d] GHR update spec pc=%x ghr=%x->%x\n", GTimer(), RegEnable(io.pc.bits, io.pc.valid), ghr, new_ghr)
+    BPUDebug(true.B, "[%d] GHR update spec pc=%x ghr=%x->%x\n", GTimer(), RegNext(io.pc.bits), ghr, new_ghr)
   }
   ghr := new_ghr
+
+  // trace
+  // when ((io.btb.hit && io.btb.entry._type === BTBtype.B && RegNext(io.pc.valid)) || RegNext(io.update.valid)) {
+  //   printf(
+  //     "%b,%b,%b,%b,%b,%b,%b,%b,%b,%b,%b\n",
+  //     imp.io.pc,              // pc
+  //     imp.io.taken,           // taken
+  //     imp.io.train_pc,        // train_pc
+  //     imp.io.train_taken,     // train_taken
+  //     imp.io.pht_rdata,       // pht_rdata
+  //     imp.io.pht_wdata,       // pht_wdata
+  //     imp.io.pht_raddr,       // pht_raddr
+  //     imp.io.pht_waddr,       // pht_waddr
+  //     imp.io.ghr_rdata,       // ghr_rdata
+  //     imp.io.ghr_wdata,       // ghr_wdata
+  //     imp.io.train_ghr_rdata  // train_ghr_rdata
+  //   )
+  // }
 }
